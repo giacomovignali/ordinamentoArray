ddd05df6c387682f08c097af6b7212f63d982c6a
Giacomo Vignali <GIACOMO.VIGNALI@local.ittcesena.it>
2021-10-08 08:35:21 +0200

    aggiunta dei commenti sull'algoritmo

--- OrdinamentoVettori/OrdinamentoVettori/Program.cs
+++ OrdinamentoVettori/OrdinamentoVettori/Program.cs
@@ -6,17 +6,20 @@ namespace OrdinamentoVettori
     {
         static void Main(string[] args)
         {
+            //creazione e riempimento del vettore con numeri interi 
             int [] vettore = new int [10] { 11, 55, -9, 7, 74, 147, -87, 6, 0, 18};
 
-
+            //metodo per ordinare i numeri del vettore
             ordina(vettore);
 
+            //ciclo per stampare i numeri in ordine crescente
             for (int i = 0; i < 10; i++)
                 Console.WriteLine(vettore[i]);
 
             Console.ReadKey();
         }
 
+        //metodo per l'ordinamento dei numeri del vettore
         static void ordina (int[] vettore)
         {
             Array.Sort(vettore);
